e95c8374598a8f379943767201493c3339872d57
Akanael21 <novaltiscorp@gmail.com>
2025-08-10 17:37:21 +0100

    feat: Automate Berry points calculation and add repair script

--- backend/render-start.sh
+++ backend/render-start.sh
@@ -5,5 +5,8 @@ set -o errexit
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Recalculating all Berry Points..."
+python manage.py recalculate_berry_points
+
 echo "Starting Gunicorn server..."
 gunicorn config.wsgi:application
